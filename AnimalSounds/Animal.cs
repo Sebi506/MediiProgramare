07ca6d4800452e2366d90e130112bfbfa0db4b0b
Sebi506 <seby506@yahoo.com>
2024-03-28 20:00:00 +0200

    AnimalSounds initial implementation
    I have implemented the initial version of the AnimalSounds project

---
+++ AnimalSounds/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSounds
+{
+    internal class Animal
+    {
+        private string[] animalList = { "cat", "dog", "fish", "owl", "pikachu"};
+        private string[] animalsSound = { "meow", "ham", "...", "hoot", "pika" };
+
+        public void EchoAnimalSound(int position)
+        {
+            Console.WriteLine("The " + animalList[position]+ " said "+ animalsSound[position]+" !");
+        }
+    }
+}
---
+++ AnimalSounds/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSounds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal misteryAnimal= new Animal();
+
+            misteryAnimal.EchoAnimalSound(0);
+            misteryAnimal.EchoAnimalSound(1);
+            misteryAnimal.EchoAnimalSound(2);
+            misteryAnimal.EchoAnimalSound(3);
+            misteryAnimal.EchoAnimalSound(4);
+
+            Console.Read();
+        }
+    }
+}
